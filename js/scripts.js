63874521734146abbfe50c262f19b2513e9709f8
sharonhalliday <hallidaysharon5@gmail.com>
2023-11-30 18:30:12 -0800

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+$(".click").click(function () {
+$("#js-showing").toggle();
+$("#js-hidden").toggle();
+});
+
+// $(".clickclick").click(function () {
+// $("#operator-showing").toggle();
+// $("#operator-hidden").toggle();
+// });
+});
